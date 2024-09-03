e6f88187930e5bea95ed691da33324d9c9078785
Myname <muthuswamynadar25@gmail.com>
2024-09-03 19:08:07 +0530

    addez

--- Learning/BaseSQL.sql
+++ Learning/BaseSQL.sql
@@ -19,7 +19,14 @@ Create table employee_salary
 	dept_id int
 );
 
-
+Insert into employee_demographics(employee_id,first_name,last_name,age,gender,birth_date)
+values
+(1,'Leslie','Knope',44,'Female','1979-09-25'),
+(3,'Tom','Haverford',36,'Male','1987-03-04'),
+(4,'April','Ludgate',61,'Male','1994-03-27'),
+(5,'Jerry','Gergich',61,'Male','1962-08-28'),
+(6,'Donna','Meagle',46,'Female','1977-07-30'),
+(9,'Ben','Wyatt',38,'Male','1985-03-25');
 
 
 
