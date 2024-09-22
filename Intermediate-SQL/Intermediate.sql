51f6a081c92d725c3453a73931da65ac0214c54e
MuthuswamyNadar <muthuswamynadar25@gmail.com>
2024-09-22 11:37:07 +0530

    added the projects

--- Intermediate-SQL/Intermediate.sql
+++ Intermediate-SQL/Intermediate.sql
@@ -43,38 +43,104 @@ inner join parks_departments as pd
 	on sal.dept_id=pd.department_id
 ;
 
+-- unions 
+
+select first_name, last_name
+from employee_demographics
+union 
+select first_name, last_name
+from employee_salary;
+
+-- using union disticnt
+
+select first_name,last_name
+from employee_demographics
+union distinct
+select first_name,last_name
+from employee_demographics;
+
+-- using union all
+
+select first_name, last_name
+from employee_demographics
+union all
+select first_name,last_name
+from employee_demographics;
+
+-- Example 1
+
+select first_name,last_name,'old' as label
+from employee_demographics
+where age>50;
+
+-- Example 2
+
+select first_name,last_name,'old' as label
+from employee_demographics
+where age >50
+union
+select first_name,last_name, 'Highly paid employee ' as label
+from employee_salary
+where salary >70000
+;
+
+-- Example 3
+
+select first_name,last_name,'old man' as label
+from employee_demographics
+where age >40 and gender ='Male'
+Union
+select first_name,last_name,'old lady 'as label
+from employee_demographics
+where age>40 and gender='Female'
+Union
+Select first_name,last_name,'Highly Paid employee' as label
+from employee_salary
+where salary>30000
+;
+
+-- String function
+
+select Length('skyfall');
 
 
+-- using string in queries
 
+select first_name,length(first_name)
+from employee_demographics
+order by 2;
 
+-- using the lower & upper function
 
+select upper('sky');
+select lower ('skyAAAAA');
 
+-- using TRIM function
 
+select trim('		sky			');
 
+-- using the LTRIM function
 
+select LTRIM('   SKY  ');
 
+-- using the right trim
 
+select RTRIM('skybird           ');
 
+-- using left & right 
 
+select first_name,
+left(first_name,4),
+right(first_name,4)
+from employee_demographics;
 
+-- substring
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+select first_name,
+left(first_name,4),
+right(first_name,4),
+substring(first_name,3,2),
+birth_date,
+substring(birth_date,6,2) as birth_month
+from employee_demographics;
 
