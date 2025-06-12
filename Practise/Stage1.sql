375139afc298b0da59539243feed3e99af8bc575
muthuswamy <muthuswamynadar25@gmail.com>
2025-06-12 23:40:59 +0530

    added

---
+++ Practise/Stage1.sql
@@ -0,0 +1,81 @@
+use muthu;
+
+
+-- *********************************************************************************************************Creating the tables *******************************************************
+
+create table employees(
+id serial primary key,
+name varchar(50),
+age int,
+department varchar (30),
+salary numeric (10,2),
+managerid int
+);
+
+
+select * from employees;
+
+
+CREATE TABLE mlover (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    price NUMERIC(10,2) NOT NULL,
+    stock INT NOT NULL,
+    tags VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE mswamy(
+	id serial primary key,
+    name varchar(100) not null,
+    price numeric(10,2) not null,
+    stock int not null,
+    tags varchar(255) not null
+);
+
+create table mine (
+	id serial primary key,
+    namez varchar(100) not null,
+    price numeric(10,2) not null,
+    stock int not null,
+    tags varchar(255) not null
+);
+    
+select * from mine;
+
+-- ********************************************************************************************************* INserting in the tables *******************************************************
+
+
+insert into mine (id,namez,price,stock,tags) values
+(102,'Swamy',2200,2,'India'),
+(103,'Manish',2400,3,'India'),
+(104,'Rohit',2600,4,'India');
+
+
+select id,namez from mine;
+
+select * from mine where id = 102;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
